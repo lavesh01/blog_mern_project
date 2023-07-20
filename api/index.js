c3b583c8c8c76aae1129e68111923cbb77833e40
Lave <laveshbisht01@gmail.com>
2023-07-21 01:16:45 +0530

    Added create post page

--- api/index.js
+++ api/index.js
@@ -4,14 +4,18 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 var jwt = require('jsonwebtoken');
+const multer  = require('multer');
+const fs = require('fs');
 
+const upload = multer({ dest: 'uploads/' })
 const User = require('./models/User');
+const Post = require('./models/Post');
 const app = express();
 
 app.use(cors({ credentials:true, origin:"http://localhost:3000"}));
-// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "laksjdflkajslkfjalksf";
@@ -40,7 +44,10 @@ app.post("/login", async (req,res) => {
         if(passOk){
             jwt.sign({ username, id: userDoc._id }, secret , {}, function(err, token) {
                 if(err) throw err;
-                res.cookie('token',token).json('ok');
+                res.cookie('token',token).json({
+                    id : userDoc._id,
+                    username: username
+                });
             });
         }else{
             res.status(400).json("Wrong Credentials");
@@ -59,6 +66,45 @@ app.get("/profile", (req,res) => {
 app.get("/logout", (req,res) => {
     res.cookie('token','').json('ok');
 });
+
+app.post('/post', upload.single('file'), async (req,res) => {
+    const {title, summary , content} = req.body;
+    const { originalname , path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length -1];
+    const newPath =  path+'.'+ext; 
+    fs.renameSync(path, newPath);
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err,info) => {
+        if (err) throw err;
+        
+        const PostDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id
+        });
+        
+        res.json(PostDoc);
+    });
+
+});
+
+app.get('/post', async (req, res) => {
+    try {
+      const posts = await Post.find().populate('author',['username']);
+      res.json(posts);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+
+// app.get('/post', async (req,res) => {
+//     res.json(await Post.find().populate('author', ['username']));
+// });
 
 app.listen(4000);
 
